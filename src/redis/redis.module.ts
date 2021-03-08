d18b78555d9dfc17a02d9eb06d2ede7f666185c7
Harukine <jacobhtnguyen@gmail.com>
2021-03-08 06:11:43 -0500

    registered redis cache in module

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,7 +1,27 @@
-import { Module } from '@nestjs/common';
-import { RedisController } from './redis.controller';
+import * as redisStore from 'cache-manager-redis-store';
+import {CacheInterceptor, CacheModule, Module} from '@nestjs/common';
+import {RedisController} from './redis.controller';
+import {APP_INTERCEPTOR} from "@nestjs/core";
 
 @Module({
-  controllers: [RedisController]
+    imports: [
+        CacheModule.registerAsync({
+            useFactory: () => {
+                return {
+                    store: redisStore,
+                    host: 'localhost',
+                    port: 6379,
+                }
+            },
+        }),
+    ],
+    controllers: [RedisController],
+    providers: [
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: CacheInterceptor
+        }
+    ]
 })
-export class RedisModule {}
+export class RedisModule {
+}
