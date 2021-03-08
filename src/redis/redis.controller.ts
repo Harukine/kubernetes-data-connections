67077a59e1be904b3c6edad780dfcc5edec16b39
Harukine <jacobhtnguyen@gmail.com>
2021-03-08 03:36:13 -0500

    injected redis service

--- src/redis/redis.controller.ts
+++ src/redis/redis.controller.ts
@@ -1,4 +1,8 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Inject} from '@nestjs/common';
+import {ClientProxy} from "@nestjs/microservices";
+import {REDIS_SERVICE} from "./redis.constants";
 
 @Controller('redis')
-export class RedisController {}
+export class RedisController {
+    constructor(@Inject(REDIS_SERVICE) private readonly client: ClientProxy) {}
+}
