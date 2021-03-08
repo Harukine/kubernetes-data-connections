0f4c1f832dd8a1731f514b344d22bdfb3d4e5489
Harukine <jacobhtnguyen@gmail.com>
2021-03-08 03:29:18 -0500

    generated redis controller

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { RedisController } from './redis.controller';
 
-@Module({})
+@Module({
+  controllers: [RedisController]
+})
 export class RedisModule {}
