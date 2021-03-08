dea8f8492416e2c687038886364a28a06eeb44c3
Harukine <jacobhtnguyen@gmail.com>
2021-03-08 06:13:07 -0500

    created basic get and set routes

--- src/redis/redis.controller.ts
+++ src/redis/redis.controller.ts
@@ -1,8 +1,24 @@
-import {Controller, Inject} from '@nestjs/common';
-import {ClientProxy} from "@nestjs/microservices";
-import {REDIS_SERVICE} from "./redis.constants";
+import {CACHE_MANAGER, Controller, Get, Inject, Param} from '@nestjs/common';
+import {Cache} from 'cache-manager';
 
 @Controller('redis')
 export class RedisController {
-    constructor(@Inject(REDIS_SERVICE) private readonly client: ClientProxy) {}
+    constructor(
+        @Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+    @Get(':id')
+    async getKey(@Param('id') id: string) {
+        const value = await this.cacheManager.get(id);
+        if (value) {
+            return `The value of ${id} is ${value}`;
+        } else {
+            return `The key ${id} not found`;
+        }
+    }
+
+    @Get(':id/:value')
+    async setKey(@Param('id') id: string, @Param('value') value: string) {
+        await this.cacheManager.set(id, value, { ttl: 1000 })
+        return `The key ${id} has been set to ${value}`;
+    }
 }
