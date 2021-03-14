e721aad1f0c3f16b1087b0f343326c7c6e3b914e
Harukine <jacobhtnguyen@gmail.com>
2021-03-14 12:25:42 -0400

    updated to use host environments

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -8,8 +8,8 @@ import {RedisController} from './redis.controller';
             useFactory: () => {
                 return {
                     store: redisStore,
-                    host: 'localhost',
-                    port: 6379,
+                    host: process.env.REDIS_HOST,
+                    port: process.env.REDIS_PORT,
                 }
             },
         }),
