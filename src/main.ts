34a05fa7662b00acf63a764e81cc7eaba0aa9177
Harukine <jacobhtnguyen@gmail.com>
2021-03-14 16:55:09 -0400

    removed microservice

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,6 @@ async function bootstrap() {
     // web app
     const app = await NestFactory.create(AppModule);
 
-    app.connectMicroservice<MicroserviceOptions>(
-        {
-            transport: Transport.REDIS,
-            options: {
-                url: 'redis://localhost:6379',
-            },
-        });
-
-    await app.startAllMicroservicesAsync();
     await app.listen(3001);
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
