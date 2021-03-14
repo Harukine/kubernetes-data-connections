428421d5f055759b4ea13bb3e2d626fa0a717067
Harukine <jacobhtnguyen@gmail.com>
2021-03-14 17:37:57 -0400

    imported postgres configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from './redis/redis.module';
 import { PostgresModule } from './postgres/postgres.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [RedisModule, PostgresModule],
+  imports: [
+      TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: 'root',
+          database: 'test',
+          autoLoadEntities: true,
+          synchronize: true,
+      }),
+      RedisModule,
+      PostgresModule],
   controllers: [AppController],
   providers: [AppService],
 })
