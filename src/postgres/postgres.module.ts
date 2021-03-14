a927794716dc9903ea972705208d1b4759d74087
Harukine <jacobhtnguyen@gmail.com>
2021-03-14 17:38:07 -0400

    created user entity

--- src/postgres/postgres.module.ts
+++ src/postgres/postgres.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostgresController } from './postgres.controller';
 import { PostgresService } from './postgres.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "./user.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [PostgresController],
   providers: [PostgresService]
 })
