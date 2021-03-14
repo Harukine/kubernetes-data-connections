077b99ce9d7c67b19ab913dc6e38f50d84933281
Harukine <jacobhtnguyen@gmail.com>
2021-03-14 17:22:39 -0400

    generated postgres service

--- src/postgres/postgres.module.ts
+++ src/postgres/postgres.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PostgresController } from './postgres.controller';
+import { PostgresService } from './postgres.service';
 
 @Module({
-  controllers: [PostgresController]
+  controllers: [PostgresController],
+  providers: [PostgresService]
 })
 export class PostgresModule {}
