1230df1ed5a907b5cfa12e13c0aee47464246a8d
Harukine <jacobhtnguyen@gmail.com>
2021-03-14 17:19:44 -0400

    generated postgres module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from './redis/redis.module';
+import { PostgresModule } from './postgres/postgres.module';
 
 @Module({
-  imports: [RedisModule],
+  imports: [RedisModule, PostgresModule],
   controllers: [AppController],
   providers: [AppService],
 })
