c3b6144b0fbad826a75692600e629917794056f9
Harukine <jacobhtnguyen@gmail.com>
2021-03-14 22:14:18 -0400

    created user controller

--- src/postgres/postgres.controller.ts
+++ src/postgres/postgres.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post} from '@nestjs/common';
+import {PostgresService} from "./postgres.service";
+import {CreateUserDto} from "./dto/create-user.dto";
+import {User} from "./user.entity";
 
 @Controller('postgres')
-export class PostgresController {}
+export class PostgresController {
+    constructor(private readonly usersService: PostgresService) {
+    }
+
+    @Post()
+    create(@Body() createUserDto: CreateUserDto): Promise<User> {
+        return this.usersService.create(createUserDto);
+    }
+
+    @Get()
+    findAll(): Promise<User[]> {
+        return this.usersService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): Promise<User> {
+        return this.usersService.findOne(id);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string): Promise<void> {
+        return this.usersService.remove(id);
+    }
+}
