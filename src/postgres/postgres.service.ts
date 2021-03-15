43028bee9e21ca0ede056b2f860919ea477c6cd1
Harukine <jacobhtnguyen@gmail.com>
2021-03-14 22:13:14 -0400

    created service for postgres

--- src/postgres/postgres.service.ts
+++ src/postgres/postgres.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {User} from "./user.entity";
+import {Repository} from "typeorm";
+import {CreateUserDto} from "./dto/create-user.dto";
 
 @Injectable()
-export class PostgresService {}
+export class PostgresService {
+    constructor(
+        @InjectRepository(User)
+        private readonly usersRepository: Repository<User>,
+    ) {
+    }
+
+    create(createUserDto: CreateUserDto): Promise<User> {
+        const user = new User();
+        user.firstName = createUserDto.firstName;
+        user.lastName = createUserDto.lastName;
+
+        return this.usersRepository.save(user);
+    }
+
+    async findAll(): Promise<User[]> {
+        return this.usersRepository.find();
+    }
+
+    findOne(id: string): Promise<User> {
+        return this.usersRepository.findOne(id);
+    }
+
+    async remove(id: string): Promise<void> {
+        await this.usersRepository.delete(id);
+    }
+}
